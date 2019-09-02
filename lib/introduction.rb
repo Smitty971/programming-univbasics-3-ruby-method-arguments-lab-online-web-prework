82be7857326494bb05fd7b7aa321cd5453dfba70
Jamal Turner <jlsmitty2273@yahoo.com>
2019-09-02 04:34:06 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -23,7 +23,10 @@ def introduction_with_language_optional(name, language = 'Ruby')
 end
 introduction_with_language_optional("Edwin")
 
-
+def introduction_with_language_optional(name, language = 'Python')
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language_optional("Edwin")
 
 
 
